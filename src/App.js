959cfdebd8beb46957e0e5abe61e71d6339771c1
durgesh-agrhari <durgeshita@gmail.com>
2023-09-08 14:54:19 +0530

    Screen not visisval solved remove unnessary file

--- src/App.js
+++ src/App.js
@@ -111,9 +111,9 @@ import Lttsalllearningmaterial from './Companys/LTTS/Ltts all learning material/
 
 
 //import ReactGA from "react-ga"; //14.8k (gzipped: 4.9k)
-// import ReactGA from "react-ga4";
-// const TRACKING_ID = "G-PE3WVLWX2S"; //
-// ReactGA.initialize(TRACKING_ID);
+import ReactGA from "react-ga4";
+const TRACKING_ID = "G-PE3WVLWX2S"; //
+ReactGA.initialize(TRACKING_ID);
 // ReactGA.pageview(document.location.pathname);
 
 function App() {
@@ -128,7 +128,7 @@ function App() {
   }, [theme])
 
 
-  // ///This is helpes to send page view to google analisis
+  //This is helpes to send page view to google analisis
   // useEffect(() => {
   //   ReactGA.pageview(window.location.pathname);
   // },[])
